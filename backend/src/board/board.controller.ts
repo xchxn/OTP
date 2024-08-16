5d86a78c2536c7b8509ee5b70f18d06634ea2b31
xchxn <seokregi@gmail.com>
2024-08-16 15:40:52 +0900

    Base setting

--- backend/src/board/board.controller.ts
+++ backend/src/board/board.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { BoardService } from './board.service';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
+  @Get()
+  getBoardList(): any {
+    return this.boardService.getPostingList();
+  }
+
+  @Post()
+  createPosting(): any {
+    return this.boardService.createPosting();
+  }
+
+  @Post()
+  updatePosting(): any {
+    return this.boardService.updatePosting();
+  }
+
+  @Post()
+  deletePosting(): any {
+    return this.boardService.deletePosting();
+  }
+}
--- backend/src/board/board.service.ts
+++ backend/src/board/board.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { PostingEntity } from './entities/posting.entity';
 @Injectable()
-export class BoardService {}
+export class BoardService {
+  constructor(
+    @Inject('POSTING_REPOSITORY')
+    private postingRepository: Repository<PostingEntity>,
+  ) {}
+  async getPostingList(): Promise<any> {
+    return 'Hello World!';
+  }
+
+  async createPosting(): Promise<any> {
+    return true;
+  }
+
+  async updatePosting(): Promise<any> {
+    return true;
+  }
+
+  async deletePosting(): Promise<any> {
+    return true;
+  }
+}
